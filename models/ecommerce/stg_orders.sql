919a8c03f56ad6c9c1c84b204a6f70aafed194ca
Michael Ye <michael.ye@thoughtspot.com>
2022-10-04 06:54:37 +0000

    restore order dates

--- models/ecommerce/stg_orders.sql
+++ models/ecommerce/stg_orders.sql
@@ -4,7 +4,8 @@ source as (
 
     select * from {{ source('ecommerce', 'orders') }}
 
-
+--- data runs to 2026, truncate timespan to desired range, current time as default
+    where ordered_at <= {{ var('truncate_timespan_to') }}
 ),
 
 renamed as (
